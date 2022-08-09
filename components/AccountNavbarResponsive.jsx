6ed6371f8314bf3bd390c881f19e0bb7ef77e249
Jose Parga Tolano <parga.jose@outlook.com>
2022-08-09 08:48:47 -0500

    prepull

--- components/AccountNavbarResponsive.jsx
+++ components/AccountNavbarResponsive.jsx
@@ -1,5 +1,5 @@
 import Link from 'next/link';
-import React, { useState } from 'react';
+import React from 'react';
 
 import styles from '../styles/AccountNavbarResponsive.module.scss';
 
@@ -23,7 +23,7 @@ const AccountNavbarResponsive=()=>{
                     className={styles.link} 
                     draggable={false}
                 >
-                    <Link href='/account/security'>Exit</Link>
+                    <Link href='/'>Exit</Link>
                 </li>
             </ul>
         </nav>
